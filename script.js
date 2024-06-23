94d2650782b6c7174504004df4fe4bcf469be26b
Luis Felipe <luis.25.ribeiro@hotmail.com>
2024-06-23 03:06:42 -0300

    deleted:    equipe.html 	new file:   imagens/amsterdam-59-c.jpg 	new file:   imagens/brasil-bandeira-cidade-scaled.jpg 	renamed:    harmoniza.jpg -> imagens/harmoniza.jpg 	renamed:    imagem 1.jpeg -> imagens/imagem 1.jpeg 	renamed:    imagem 2.jpeg -> imagens/imagem 2.jpeg 	renamed:    imagem.jpg -> imagens/imagem.jpg 	new file:   imagens/lisboa.jpg 	renamed:    logotipo-clinica-de-estetica.png -> imagens/logotipo-clinica-de-estetica.png 	new file:   imagens/paris.jpg 	renamed:    rosto mulher.jpeg -> imagens/rosto mulher.jpeg 	modified:   index.html 	modified:   login.html 	modified:   paises.html 	modified:   procedimentos.html 	modified:   script.js 	modified:   style.css 	deleted:    style.css2 	deleted:    style.csspaises 	new file:   stylelogin.css 	new file:   stylepaises.css

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
     $('.slider').slider();
   });
+
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
